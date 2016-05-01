09955eaf12c1a51db3302ef67a213c6f760ca642
Michael G. Ströck <michael@stroeck.com>
2016-05-01 23:32:37 +0200

    Add some commone FB Graph API functionality.

---
+++ app/controllers/facebookOpenGraph.js
@@ -0,0 +1,44 @@
+console.log('OIDA');
+module.exports = function() {
+
+    function getUserName(sender) {
+        request({
+            url: 'https://graph.facebook.com/v2.6/' + sender,
+            qs: {
+                access_token: app.get('page_access_token'),
+                fields: "first_name,last_name"
+            },
+            method: 'GET',
+        }, function(error, response, body) {
+            if (error) {
+                console.log('Error sending messages: ', error)
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error)
+            }
+        }).on('data', function(data) {
+            return JSON.parse(data)
+        })
+    }
+
+    function sendTextMessage(sender, text) {
+        messageData = {
+            text:text
+        }
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {access_token:app.get('page_access_token')},
+            method: 'POST',
+            json: {
+                recipient: {id: sender},
+                message: messageData,
+            }
+        }, function(error, response, body) {
+            if (error) {
+                console.log('Error sending messages: ', error)
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error)
+            }
+        })
+    }
+
+};
