60206bcc9571c6e1cb7444928291400038579504
Michael G. Ströck <michael@stroeck.com>
2016-05-01 21:52:35 +0200

    Add dotenv module.

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ var config = {
       name: 'chatbot'
     },
     port: process.env.PORT || 3000,
-    db: 'postgres://localhost/chatbot-development'
+    db: 'postgres://beavr_prdctn_u:hightail-vermeil-jeep-nostrum-cuba-omega@beavr-prdctn.cfiqbxj1zyb4.eu-west-1.rds.amazonaws.com/chancenreich'
   },
 
   test: {
