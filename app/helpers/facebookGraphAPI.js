3e3b07f890fff2a3f0dea5ee72c7ae88226fc41c
Michael G. Ströck <michael@stroeck.com>
2016-05-10 12:37:26 +0000

    Start using promises for return values of FB helper classes.

--- app/helpers/facebookGraphAPI.js
+++ app/helpers/facebookGraphAPI.js
@@ -1,25 +1,21 @@
 var config = require('../../config/config'),
     request = require('request');
+    rp = require('request-promise');
 
 module.exports = {
 
     getUserName: function getUserName(sender) {
-        request({
-            url: 'https://graph.facebook.com/v2.6/' + sender,
-            qs: {
-                access_token: config.page_access_token,
-                fields: "first_name,last_name"
-            },
-            method: 'GET'
-        }, function(error, response, body) {
-            if (error) {
-                console.log('Error sending messages: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            }
-        }).on('data', function(data) {
-            return JSON.parse(data)
-        })
+        return  rp({
+                    url: 'https://graph.facebook.com/v2.6/' + sender,
+                    qs: {
+                        access_token: config.page_access_token,
+                        fields: "first_name,last_name"
+                    },
+                    method: 'GET'
+                })
+                .then (function(data) {
+                    return JSON.parse(data);
+                })
     },
 
     sendTextMessage: function sendTextMessage(sender, text) {
